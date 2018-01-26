9b35edb4941589cc5979a7458088d6f4fae05327
Casey Kelly <casey.kelly@colorado.edu>
2018-01-25 20:32:48 -0700

    prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
-import promise from 'redux-promise';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from "react-router-dom";
+import promise from "redux-promise";
 
-import reducers from './reducers';
-import PostsIndex from './components/posts_index';
+import reducers from "./reducers";
+import PostsIndex from "./components/posts_index";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,8 +14,9 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex}/>
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector(".container")
+);

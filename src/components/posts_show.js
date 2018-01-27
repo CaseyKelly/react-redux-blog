a5e131da9dc221665d57719830fc106821e85600
Casey Kelly <casey.kelly@colorado.edu>
2018-01-27 13:37:03 -0700

    render post data to post_show

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -5,11 +5,23 @@ import { fetchPost } from "../actions";
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
-    this.props.fetchPost();
+    this.props.fetchPost(id);
   }
 
   render() {
-    return <div>Posts Show!</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
   }
 }
 
